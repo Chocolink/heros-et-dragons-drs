3a3b47d518e752d3a524eef4776ab59b82965889
ISC Lab's user <isc@mb-18.sofr.hefr.lan>
2021-03-09 13:34:15 +0100

    add macOS appli

--- docs/.vuepress/theme/util/homebrew.js
+++
@@ -1,7 +0,0 @@
-export function encode (o) {
-  return btoa(unescape(encodeURIComponent(JSON.stringify(o))))
-}
-
-export function decode (s) {
-  return JSON.parse(decodeURIComponent(escape(atob(s))))
-}
